3daa89fab9b51578101c0c031e692ca949cc0844
dr-ak <alex-r-1982@mail.ru>
2021-06-07 11:41:01 +0700

    1.12. Начинаем программировать

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import MainPage from '../main-page/main-page';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App(props) {
+  const {title, date} = props.data;
+  return (
+    <MainPage title = {title} date = {date}/>
+  );
 }
+
+App.propTypes = {
+  data: PropTypes.object.isRequired,
+};
 
 export default App;
--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const data = {
+  title: 'The Grand Budapest Hotel',
+  date: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App data = {data}/>
   </React.StrictMode>,
   document.getElementById('root'));
