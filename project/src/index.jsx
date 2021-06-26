184242ff34065551778823906cf0b285dc6f2dc3
dr-ak <alex-r-1982@mail.ru>
2021-06-26 18:49:22 +0700

    creating api

--- project/src/index.jsx
+++ project/src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {createAPI} from './services/api';
 import {Provider} from 'react-redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 
